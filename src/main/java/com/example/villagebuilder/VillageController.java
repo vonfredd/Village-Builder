70c7031d91f8422c7bf7e60597d716b632a64803
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-01-11 17:02:52 +0100

    Update HelloApplication, HelloController to Main and VillageController. Update hello-view.fxml to village.fxml

--- src/main/java/com/example/villagebuilder/VillageController.java
+++ src/main/java/com/example/villagebuilder/VillageController.java
@@ -3,7 +3,7 @@ package com.example.villagebuilder;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class VillageController {
     @FXML
     private Label welcomeText;
 
