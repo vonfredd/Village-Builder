f042e100b84ab329723ba3d588155004f7376500
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-01-11 16:39:38 +0100

    Remove EconomicBuildingFactory to replace with EconomicBuilding

--- src/main/java/com/example/villagebuilder/buildings/EconomicBuildingFactory.java
+++
@@ -1,8 +0,0 @@
-package com.example.villagebuilder.buildings;
-
-public class EconomicBuildingFactory implements BuildingFactory{
-    @Override
-    public Building createBuilding() {
-        return new EconomicBuilding();
-    }
-}
