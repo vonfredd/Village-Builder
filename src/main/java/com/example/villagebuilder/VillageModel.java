de2af7594ab9c78c6a782482226031756052a8b8
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-01-21 21:42:12 +0100

    Updated fields with new List,Add  callForProduction

--- src/main/java/com/example/villagebuilder/VillageModel.java
+++ src/main/java/com/example/villagebuilder/VillageModel.java
@@ -1,10 +1,22 @@
 package com.example.villagebuilder;
 
 import com.example.villagebuilder.buildings.*;
+import com.example.villagebuilder.production.ResourceProduction;
+import javafx.collections.FXCollections;
+
+import java.util.List;
 
 public class VillageModel {
 
-    public void buildBuildings(){
+    private List<Building> buildings = FXCollections.observableArrayList();
+
+    public void constructBuilding(){
+        //Add some code to add a building and make ResourceProduction builings list also see all buildings.
+
+    }
+
+    public void callForProduction(ResourceProduction resourceProduction){
+        resourceProduction.baseProduction(buildings);
     }
 
 }
