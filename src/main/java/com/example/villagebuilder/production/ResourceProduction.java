8c3829e4d009c1a5a24f860a0e9c1583ec76c8f2
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-01-21 17:31:21 +0100

    Add resource amount as fields in class. Add List of buildings to see what buildings player has in village for production. Add startProductionMethod.

--- src/main/java/com/example/villagebuilder/production/ResourceProduction.java
+++ src/main/java/com/example/villagebuilder/production/ResourceProduction.java
@@ -1,4 +1,23 @@
 package com.example.villagebuilder.production;
 
+import com.example.villagebuilder.buildings.Building;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+
+import java.util.List;
+
 public class ResourceProduction {
+
+    //lumber brick wheat
+    private int lumberAmount;
+    private int brickAmount;
+    private int wheatAmount;
+
+    private List <Building> buildings = FXCollections.observableArrayList();
+
+    public void startProduction(){
+        lumberAmount += 1;
+        brickAmount += 1;
+        wheatAmount += 1;
+    }
 }
