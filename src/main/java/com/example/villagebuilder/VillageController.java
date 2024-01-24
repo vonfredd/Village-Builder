e57299af9ab0ba6ed6f777cb869787aa400ad26c
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-01-24 23:07:26 +0100

    Add switch statement to check what constructionsite was clicked

--- src/main/java/com/example/villagebuilder/VillageController.java
+++ src/main/java/com/example/villagebuilder/VillageController.java
@@ -5,10 +5,44 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Circle;
 import javafx.util.Duration;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class VillageController {
+    @FXML
+    private Circle siteOne;
+    @FXML
+    private Circle siteTwo;
+    @FXML
+    private Circle siteThree;
+    @FXML
+    private Circle siteFour;
+    @FXML
+    private Circle siteFive;
+    @FXML
+    private Circle siteSeven;
+    @FXML
+    private Circle siteSix;
+    @FXML
+    private ImageView siteOneImage;
+    @FXML
+    private ImageView siteTwoImage;
+    @FXML
+    private ImageView siteThreeImage;
+    @FXML
+    private ImageView siteFourImage;
+    @FXML
+    private ImageView siteFiveImage;
+    @FXML
+    private ImageView siteSixImage;
+    @FXML
+    private ImageView siteSevenImage;
     @FXML
     private Label brickLabel;
     @FXML
@@ -17,10 +51,12 @@ public class VillageController {
     private Label wheatLabel;
     private ResourceProduction resourceProduction;
     private VillageModel model = new VillageModel();
+    private List<Circle> constructionSites;
 
     public void initialize() {
         startingTheTimeline();
         resourceProduction = new ResourceProduction();
+        constructionSites = Arrays.asList(siteOne,siteTwo,siteThree,siteFour,siteFive,siteSix,siteSeven);
         brickLabel.textProperty().bind(resourceProduction.brickAmountProperty().asString());
         lumberLabel.textProperty().bind(resourceProduction.lumberAmountProperty().asString());
         wheatLabel.textProperty().bind(resourceProduction.wheatAmountProperty().asString());
@@ -44,5 +80,18 @@ public class VillageController {
 
 
     public void constructionSite(MouseEvent mouseEvent) {
+        Circle circle = (Circle) mouseEvent.getSource();
+        switch (circle.getId()){
+            case"siteOne" -> System.out.println(1);
+            case "siteTwo" -> System.out.println(2);
+            case "siteThree" -> System.out.println(3);
+            case "siteFour" -> System.out.println(4);
+            case "siteFive" -> System.out.println(5);
+            case "siteSix" -> System.out.println(6);
+            case "siteSeven" -> System.out.println(7);
+        }
+
     }
+
+
 }
