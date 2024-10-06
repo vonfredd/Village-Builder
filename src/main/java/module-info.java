7f90d585cd1b1d4ce1513ac187a0a9e3bc163b8f
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-10-06 21:25:39 +0200

    Refactor

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module com.example.villagebuilder {
 
     opens com.example.villagebuilder to javafx.fxml;
     exports com.example.villagebuilder;
+    exports com.example.villagebuilder.buildings;
+    exports com.example.villagebuilder.production;
 }
