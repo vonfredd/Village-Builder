03c4321c5b3af2563048fe8e5f0b864cc05dee6c
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-10-06 21:25:09 +0200

    Add methods to override

--- src/main/java/com/example/villagebuilder/buildings/Building.java
+++ src/main/java/com/example/villagebuilder/buildings/Building.java
@@ -3,4 +3,8 @@ package com.example.villagebuilder.buildings;
 public interface Building{
     int produceResource();
     String getType();
+    int getWheatPrice();
+    int getBricksPrice();
+    int getLumberPrice();
+    int getLevel();
 }
